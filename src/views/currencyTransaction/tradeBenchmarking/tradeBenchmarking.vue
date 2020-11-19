3abc4bb49cd71cd1c86006d967b58a85aae425b5
wumaoxia <1805428335@qq.com>
2020-11-19 19:33:54 +0800

    提交

--- src/views/currencyTransaction/tradeBenchmarking/tradeBenchmarking.vue
+++ src/views/currencyTransaction/tradeBenchmarking/tradeBenchmarking.vue
@@ -1,8 +1,11 @@
 <!--
  * @Author: your name
  * @Date: 2020-11-19 15:50:11
- * @LastEditTime: 2020-11-19 16:04:51
+ * @LastEditTime: 2020-11-19 19:33:38
  * @LastEditors: Please set LastEditors
  * @Description: 交易对标
  * @FilePath: \exChange\src\views\currencyTransaction\tradeBenchmarking\tradeBenchmarking.vue
 -->
+<template>
+    <div>交易对标</div>
+</template>
